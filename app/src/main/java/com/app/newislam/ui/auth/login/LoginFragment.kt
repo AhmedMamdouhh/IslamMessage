f7754f4b67869e4d2097d6eda4e05ef8042df5f9
mohameddawood94 <mo7ameddawod@gmail.com>
2020-10-26 22:23:33 +0200

    add laoding animation

--- app/src/main/java/com/app/newislam/ui/auth/login/LoginFragment.kt
+++ app/src/main/java/com/app/newislam/ui/auth/login/LoginFragment.kt
@@ -6,9 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import com.app.newislam.R
 import com.app.newislam.databinding.FragmentLoginBinding
+import com.app.newislam.model.Resource
 import com.app.newislam.model.requests.auth.login.LoginRequest
+import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.toolbar.view.*
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -27,7 +31,24 @@ class LoginFragment : Fragment() {
         binding = FragmentLoginBinding.inflate( inflater,container,false)
         binding.userObject = userRequest
         binding.viewModel = loginViewModel
+        binding.toolbar.tv_title.text = "Login"
 
+        loginViewModel.resource.observe(viewLifecycleOwner, Observer {
+           if (it!=null){
+               when(it.resourceStatus){
+                   Resource.ResourceStatus.SUCCESS->{
+                       loadingView.visibility = View.GONE
+                   }
+                   Resource.ResourceStatus.LOADING->{
+                       loadingView.visibility = View.VISIBLE
+                       loadingView.playAnimation()
+                   }
+                   Resource.ResourceStatus.FAILED->{
+                       loadingView.visibility = View.GONE
+                   }
+               }
+           }
+        })
         return binding.root
     }
 }
