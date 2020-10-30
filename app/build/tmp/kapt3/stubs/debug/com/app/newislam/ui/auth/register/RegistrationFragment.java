14fa583a3822549ba88cb3e9c66ce0cc4698f4b1
mohameddawood94 <mo7ameddawod@gmail.com>
2020-10-30 13:40:34 +0200

    add register,forget password

---
+++ app/build/tmp/kapt3/stubs/debug/com/app/newislam/ui/auth/register/RegistrationFragment.java
@@ -0,0 +1,46 @@
+package com.app.newislam.ui.auth.register;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000<\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J&\u0010\u0014\u001a\u0004\u0018\u00010\u00152\u0006\u0010\u0016\u001a\u00020\u00172\b\u0010\u0018\u001a\u0004\u0018\u00010\u00192\b\u0010\u001a\u001a\u0004\u0018\u00010\u001bH\u0016R\u001a\u0010\u0003\u001a\u00020\u0004X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0005\u0010\u0006\"\u0004\b\u0007\u0010\bR\u001b\u0010\t\u001a\u00020\n8FX\u0086\u0084\u0002\u00a2\u0006\f\n\u0004\b\r\u0010\u000e\u001a\u0004\b\u000b\u0010\fR\u001b\u0010\u000f\u001a\u00020\u00108FX\u0086\u0084\u0002\u00a2\u0006\f\n\u0004\b\u0013\u0010\u000e\u001a\u0004\b\u0011\u0010\u0012\u00a8\u0006\u001c"}, d2 = {"Lcom/app/newislam/ui/auth/register/RegistrationFragment;", "Lcom/app/newislam/manager/base/BaseFragment;", "()V", "binding", "Lcom/app/newislam/databinding/FragmentRegisterBinding;", "getBinding", "()Lcom/app/newislam/databinding/FragmentRegisterBinding;", "setBinding", "(Lcom/app/newislam/databinding/FragmentRegisterBinding;)V", "registrationRequest", "Lcom/app/newislam/model/requests/auth/register/RegistrationRequest;", "getRegistrationRequest", "()Lcom/app/newislam/model/requests/auth/register/RegistrationRequest;", "registrationRequest$delegate", "Lkotlin/Lazy;", "viewModel", "Lcom/app/newislam/ui/auth/register/RegisterViewModel;", "getViewModel", "()Lcom/app/newislam/ui/auth/register/RegisterViewModel;", "viewModel$delegate", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"})
+public final class RegistrationFragment extends com.app.newislam.manager.base.BaseFragment {
+    @org.jetbrains.annotations.NotNull()
+    public com.app.newislam.databinding.FragmentRegisterBinding binding;
+    @org.jetbrains.annotations.NotNull()
+    private final kotlin.Lazy viewModel$delegate = null;
+    @org.jetbrains.annotations.NotNull()
+    private final kotlin.Lazy registrationRequest$delegate = null;
+    private java.util.HashMap _$_findViewCache;
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.app.newislam.databinding.FragmentRegisterBinding getBinding() {
+        return null;
+    }
+    
+    public final void setBinding(@org.jetbrains.annotations.NotNull()
+    com.app.newislam.databinding.FragmentRegisterBinding p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.app.newislam.ui.auth.register.RegisterViewModel getViewModel() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.app.newislam.model.requests.auth.register.RegistrationRequest getRegistrationRequest() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    @java.lang.Override()
+    public android.view.View onCreateView(@org.jetbrains.annotations.NotNull()
+    android.view.LayoutInflater inflater, @org.jetbrains.annotations.Nullable()
+    android.view.ViewGroup container, @org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+        return null;
+    }
+    
+    public RegistrationFragment() {
+        super();
+    }
+}
